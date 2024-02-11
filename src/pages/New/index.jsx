c70e13776e26aa56abce29455de320b6c115f4f4
Gabriel Nascimento <127701444+bielnascf@users.noreply.github.com>
2024-02-11 20:43:10 -0300

    Adicionei rotas e terminei as pages e components

---
+++ src/pages/New/index.jsx
@@ -0,0 +1,43 @@
+import { Container, Form } from './styles'
+
+import { Header } from '../../components/Header'
+import { Input } from '../../components/Input'
+import { TextArea } from '../../components/TextArea'
+import { Section } from '../../components/Section'
+import { NoteItem } from '../../components/NoteItem'
+import { Button } from '../../components/Button'
+
+
+export function New() {
+    return (
+        <Container>
+            <Header />
+
+            <main>
+                <Form>
+                    <header>
+                        <h1>Criar Nota</h1>
+                        <a href="/">Voltar</a>
+                    </header>
+
+                    <Input placeholder="Título"/>
+                    <TextArea placeholder="Observações"/>
+
+                    <Section title="Links úteis">
+                        <NoteItem value="https://www.rocketseat.com.br"/>
+                        <NoteItem value="" isNew placeholder="Novo Link"/>
+                    </Section>
+
+                    <Section title="Marcadores">
+                        <div className="tags">
+                            <NoteItem value="react"/>
+                            <NoteItem value="" isNew placeholder="Nova Tag"/>
+                        </div>
+                    </Section>
+
+                    <Button title="Salvar"/>
+                </Form>
+            </main>
+        </Container>
+    )
+}

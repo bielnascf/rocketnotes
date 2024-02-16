658d17718659688bd8800a19332216cca786abf9
Gabriel Nascimento <127701444+bielnascf@users.noreply.github.com>
2024-02-16 00:20:20 -0300

    Comecei integraçao front e back, adicionei novas libs como axios e terminei cadastro de usuario

--- src/pages/Signup/index.jsx
+++ src/pages/Signup/index.jsx
@@ -1,10 +1,66 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-unreachable */
 import { Container, Form, Background } from './styles'
+import { useState } from 'react'
 import { FiMail, FiLock, FiUser } from 'react-icons/fi'
+import Swal from 'sweetalert2'
 import { Input } from '../../components/Input'
 import { Button } from '../../components/Button'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
+import { api } from '../../services/api'
 
 export function Signup() {
+    const [name, setName] = useState("")
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+
+    const navigate = useNavigate()
+
+    function handleSignUp() {
+        if(!name || !email || !password) {
+            return Swal.fire({
+                icon: "error",
+                title: "Oops...",
+                text: "Preencha todos os campos!",
+                background: "#3E3B47",
+                color: "#ffff",
+                confirmButtonColor: "#FF9000"
+            });
+        }
+
+        api.post('/users', { name, email, password })
+        .then(() => {
+            Swal.fire({
+                icon: "success",
+                title: "Success",
+                text: "Usuário cadastrado com sucesso!",
+                background: "#3E3B47",
+                color: "#ffff",
+                confirmButtonColor: "#FF9000"
+            });
+        })
+        .catch(error => {
+            if(error.response) {
+                Swal.fire({
+                    icon: "error",
+                    title: "Oops...",
+                    text: error.response.data.message,
+                    background: "#3E3B47",
+                    color: "#ffff",
+                    confirmButtonColor: "#FF9000"
+                });
+            } else {
+                Swal.fire({
+                    icon: "error",
+                    title: "Oops...",
+                    text: "Não foi possível cadastrar",
+                    background: "#3E3B47",
+                    color: "#ffff",
+                    confirmButtonColor: "#FF9000"
+                });
+            }
+        })
+    }
     return (
         <Container>
             <Background />
@@ -19,21 +75,26 @@ export function Signup() {
                     placeholder="Nome"
                     type="text"
                     icon={FiUser}
+                    onChange={e => setName(e.target.value)}
                 />
 
                 <Input 
                     placeholder="E-mail"
                     type="text"
                     icon={FiMail}
+                    onChange={e => setEmail(e.target.value)}
+                    
                 />
 
                 <Input 
                     placeholder="Senha"
                     type="password"
                     icon={FiLock}
+                    onChange={e => setPassword(e.target.value)}
+
                 />
 
-                <Button title="Cadastrar"/>
+                <Button title="Cadastrar" onClick={handleSignUp}/>
 
                 <Link to="/">
                     Voltar para o login
